e7041412da5bd45b631065fef741433d046cbcea
major sapp <majorflght02@gmail.com>
2016-01-26 22:22:26 -0500

    added some more field options to ModelAdmin class.

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+	list_display = ['title', 'updated', 'timestamp']
+	list_display_links = ['updated']
+	list_filter = ['updated', 'timestamp']
+	search_fields = ['content', 'title']
+	list_editable = ['title']
+	class Meta:
+		model = Post
